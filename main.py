9d860d8f3e8ac6ef06f20ace624230f35e33765b
DevAsper <asponomaryov@yandex.ru>
2024-05-13 14:46:28 +0300

    add func and button complete order

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
-
 import sqlite3
+from tkinter import messagebox
 
 
 def init_db():
@@ -39,6 +39,18 @@ def view_orders():
         tree.insert("", tk.END, values=row)
     conn.close()
 
+def complete_order():
+    selected_item = tree.selection()
+    if selected_item:
+        order_id = tree.item(selected_item, "values")[0]
+        conn = sqlite3.connect('business_orders.db')
+        cur = conn.cursor()
+        cur.execute("UPDATE orders SET status = 'Завершен' WHERE id = ?", (order_id,))
+        conn.commit()
+        conn.close()
+        view_orders()
+    else:
+        messagebox.showerror("Ошибка", "Выберите заказ для завершения")
 
 app = tk.Tk()
 app.title("Система управления заказами")
@@ -55,6 +67,9 @@ order_details.pack()
 
 add_button = tk.Button(app, text="Добавить заказ", command=add_order)
 add_button.pack()
+
+complete_button = tk.Button(app, text="Завершить заказ", command=complete_order)
+complete_button.pack()
 
 columns = ("id", "customer_name", "order_details", "status")
 tree = ttk.Treeview(app, columns=columns, show="headings")
